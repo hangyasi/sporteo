341314f5c327141dcec8fec3dee3dbe5ff112ebc
Zoltan Hangyasi <hangyasi.zoltan@gmail.com>
2018-12-09 17:58:45 +0100

    [CLIENT] user management and interceptor implemented

--- sporteo-web/src/app/app.module.ts
+++ sporteo-web/src/app/app.module.ts
@@ -6,6 +6,11 @@ import { LoginComponent } from './login/login.component';
 import { AddUserComponent } from './add-user/add-user.component';
 import { EditUserComponent } from './edit-user/edit-user.component';
 import { ListUserComponent } from './list-user/list-user.component';
+import {ApiService} from "./core/api.service";
+import {HTTP_INTERCEPTORS, HttpClientModule} from "@angular/common/http";
+import {ReactiveFormsModule} from "@angular/forms";
+import {routing} from "./app.routing";
+import {TokenInterceptor} from "./core/interceptor";
 
 @NgModule({
   declarations: [
@@ -16,9 +21,14 @@ import { ListUserComponent } from './list-user/list-user.component';
     ListUserComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    routing,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [ApiService, {provide: HTTP_INTERCEPTORS,
+    useClass: TokenInterceptor,
+    multi : true}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- sporteo-web/src/app/edit-user/edit-user.component.ts
+++ sporteo-web/src/app/edit-user/edit-user.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit , Inject} from '@angular/core';
+import {Router} from "@angular/router";
+import {User} from "../model/user.model";
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {first} from "rxjs/operators";
+import {ApiService} from "../core/api.service";
 
 @Component({
   selector: 'app-edit-user',
@@ -7,9 +12,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditUserComponent implements OnInit {
 
-  constructor() { }
+  user: User;
+  editForm: FormGroup;
+  constructor(private formBuilder: FormBuilder,private router: Router, private apiService: ApiService) { }
 
   ngOnInit() {
+    let userId = window.localStorage.getItem("editUserId");
+    if(!userId) {
+      alert("Invalid action.")
+      this.router.navigate(['list-user']);
+      return;
+    }
+    this.editForm = this.formBuilder.group({
+      id: [''],
+      username: ['', Validators.required],
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      age: ['', Validators.required],
+      salary: ['', Validators.required]
+    });
+    this.apiService.getUserById(+userId)
+      .subscribe( data => {
+        this.editForm.setValue(data.result);
+      });
+  }
+
+  onSubmit() {
+    this.apiService.updateUser(this.editForm.value)
+      .pipe(first())
+      .subscribe(
+        data => {
+          if(data.status === 200) {
+            alert('User updated successfully.');
+            this.router.navigate(['list-user']);
+          }else {
+            alert(data.message);
+          }
+        },
+        error => {
+          alert(error);
+        });
   }
 
 }
