e05d627a3e10c713994fc0960884d4bef4046198
Zoltan Hangyasi <hangyasi.zoltan@gmail.com>
2018-12-15 14:15:09 +0100

    [SERVER] Base Spring Boot Security skeleton

---
+++ sporteo-server/src/main/java/com/hangyasi/sporteo/SporteoApplication.java
@@ -0,0 +1,14 @@
+package com.hangyasi.sporteo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SporteoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SporteoApplication.class, args);
+	}
+
+}
+
